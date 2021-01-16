3e0e8edd2eff3015053f4f8a44c9960503700197
uzi3 <43525327+uzi3@users.noreply.github.com>
2021-01-16 10:00:45 +0530

    first commit

---
+++ Password reset.ps1
@@ -0,0 +1,6 @@
+﻿$newPassword = (Read-Host -Prompt "Provide New Password" -AsSecureString)
+$users=Get-Content D:\List.txt
+foreach ($i in $users)
+{
+Set-ADAccountPassword -Identity $i -NewPassword $newPassword -Reset
+}
